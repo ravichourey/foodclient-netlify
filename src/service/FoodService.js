b6349c887b5f484eeda8f3a71bdf7ab1852959d1
ravichourey <ravichourey779@gmail.com>
2025-05-23 20:15:39 +0530

    Updated code for backend

--- src/service/FoodService.js
+++ src/service/FoodService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/food";
+const API_URL = "https://foodiesapi-uep5.onrender.com/api/food";
 
 export const fetchFoodList = async () => {
   try {
